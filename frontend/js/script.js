b0268356461ebcaa1ced6cb5a77032c03d764ae7
FranciscoFil <ffilipeoffg@gmail.com>
2024-04-13 17:32:43 -0300

    second commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -86,7 +86,7 @@ const handleLogin = (event) => {
     login.style.display = "none" 
     chat.style.display = "flex" 
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-frontend-backend.onrender.com")
     websocket.onmessage = processMessage
     
 }
